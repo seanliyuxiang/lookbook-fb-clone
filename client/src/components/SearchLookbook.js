c5e70b370b4c39a7da9922c16eacabd22f9d6684
Yuxiang Li <seanliyuxiang@gmail.com>
2022-02-13 23:28:16 -0600

    Implemented the search users feature

---
+++ client/src/components/SearchLookbook.js
@@ -0,0 +1,39 @@
+import {useState, useEffect} from 'react';
+import SearchResultsContainer from './SearchResultsContainer';
+
+function SearchLookbook() {
+
+  const [allUsers, setAllUsers] = useState([]);
+  const [searchString, setSearchString] = useState('');
+
+  useEffect(() => {
+    fetch('/api/users')
+    .then(response => response.json())
+    .then(users => setAllUsers(users));
+  }, []);
+
+  const displayedUsers = allUsers.filter(
+    user => `${user.first_name} ${user.last_name}`.toLowerCase().includes(searchString.toLowerCase())
+  );
+
+  function changeSearchStringHandler(event) {
+    setSearchString(event.target.value);
+  }
+
+  return (
+    <div>
+      <input
+        type='search'
+        placeholder='Search L👀kbook'
+        value={searchString}
+        onChange={changeSearchStringHandler}
+      />
+      <SearchResultsContainer
+        displayedUsers={displayedUsers}
+        searchString={searchString}
+      />
+    </div>
+  );
+}
+
+export default SearchLookbook;
