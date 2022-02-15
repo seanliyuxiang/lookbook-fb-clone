9549842ec719642082bbecf9dd8a3f53db00940b
Yuxiang Li <seanliyuxiang@gmail.com>
2022-02-14 22:48:57 -0600

    Added <Link> component to user's name in the SearchResultsContainer component

--- client/src/components/SearchResultsContainer.js
+++ client/src/components/SearchResultsContainer.js
@@ -1,12 +1,14 @@
+import {Link} from 'react-router-dom';
+
 function SearchResultsContainer({displayedUsers, searchString}) {
 
-  const displayedUsersArrJSX = displayedUsers.map(
-    user => {
-      return (
-        <p key={user.id}>{`${user.first_name} ${user.last_name}`}</p>
-      );
-    }
-  );
+  const displayedUsersArrJSX = displayedUsers.map(user => {
+    return (
+      <p key={user.id}>
+        <Link to={`/users/${user.id}`}>{`${user.first_name} ${user.last_name}`}</Link>
+      </p>
+    );
+  });
   
   return (
     <div>
