c82225186edbb847f3d912b948c576c39aa1090d
Yuxiang Li <seanliyuxiang@gmail.com>
2022-04-10 22:15:17 -0500

    Added general styling for the FormToSubmitComment component

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,7 @@ import './styles/thumbs.css';
 import './styles/FormToSubmitPost.css';
 import './styles/posts.css';
 import './styles/comments.css';
+import './styles/FormToSubmitComment.css';
 
 ReactDOM.render(
   <BrowserRouter>
