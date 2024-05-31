c7ae1cf879a020873002b9fc58ddeb45aa5fbd03
seanliyuxiang <seanliyuxiang@gmail.com>
2024-05-31 15:33:19 -0500

    Added social icons in Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,3 +1,6 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 function Footer({user}) {
   // if no user is logged in
   if (!user) {
@@ -8,17 +11,17 @@ function Footer({user}) {
         </small>
         <div>
           <small className='footer-copy-logged-out'>
-            Lookbook &copy; 2022
+            Lookbook &copy; {(new Date()).getFullYear()}
           </small>
           <ul className='footer-links-logged-out'>
-            <li><a href='https://github.com/seanliyuxiang/lookbook-fb-clone' target='_blank'>GitHub</a></li>
-            <li><a href='https://www.linkedin.com/in/seanliyuxiang' target='_blank'>LinkedIn</a></li>
-            <li><a href='https://medium.com/@seanliyuxiang' target='_blank'>Medium</a></li>
+            <li><a href='https://github.com/seanliyuxiang/lookbook-fb-clone' target='_blank'><GitHubIcon fontSize='small' />GitHub</a></li>
+            <li><a href='https://www.linkedin.com/in/seanliyuxiang' target='_blank'><LinkedInIcon fontSize='small' />LinkedIn</a></li>
+            {/* <li><a href='https://medium.com/@seanliyuxiang' target='_blank'>Medium</a></li> */}
           </ul>
         </div>
         <small className='footer-tech-stack'>
-          Lookbook is a clone of Facebook built using Ruby, Rails, JavaScript, React, HTML, and CSS. 
-          Check out its <a href='https://github.com/seanliyuxiang/lookbook-fb-clone' target='_blank'>source code on my GitHub</a>!
+          Lookbook is a clone of Facebook built using Ruby, Rails, PostgreSQL, JavaScript, React, HTML, and CSS.
+          Check out its code <a href='https://github.com/seanliyuxiang/lookbook-fb-clone' target='_blank'>here</a>!
         </small>
       </footer>
     );
@@ -28,12 +31,12 @@ function Footer({user}) {
   return (
     <footer className='footer'>
       <small className='footer-copy'>
-        Lookbook &copy; 2022
+        Lookbook &copy; {(new Date()).getFullYear()}
       </small>
       <ul className='footer-links'>
-        <li><a href='https://github.com/seanliyuxiang/lookbook-fb-clone' target='_blank'>GitHub</a></li>
-        <li><a href='https://www.linkedin.com/in/seanliyuxiang' target='_blank'>LinkedIn</a></li>
-        <li><a href='https://medium.com/@seanliyuxiang' target='_blank'>Medium</a></li>
+        <li><a href='https://github.com/seanliyuxiang/lookbook-fb-clone' target='_blank'><GitHubIcon fontSize='small' />GitHub</a></li>
+        <li><a href='https://www.linkedin.com/in/seanliyuxiang' target='_blank'><LinkedInIcon fontSize='small' />LinkedIn</a></li>
+        {/* <li><a href='https://medium.com/@seanliyuxiang' target='_blank'>Medium</a></li> */}
       </ul>
     </footer>
   );
