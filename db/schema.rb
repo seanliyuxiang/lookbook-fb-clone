0ff44ac6bdd88f5182253524f12d0130286abecd
seanliyuxiang <seanliyuxiang@gmail.com>
2024-07-13 22:06:32 -0500

    Generated a Rails migration to add the 'status' column to the 'friendships' table. Updated database schema to reflect this change.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_210137) do
+ActiveRecord::Schema.define(version: 2024_07_14_025323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_210137) do
     t.integer "friend_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "pending"
   end
 
   create_table "likes", force: :cascade do |t|
