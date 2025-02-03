dfee1492673bfbfbbd6544134e863bfb720c3ef0
seanliyuxiang <seanliyuxiang@gmail.com>
2025-02-03 13:06:54 -0600

    Configured non-API requests to pass through to the client React application for the deployed application

--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,12 @@ Rails.application.routes.draw do
     post '/users/:id/attach_new_cover_photo', to: 'users#attach_new_cover_photo'
 
   end
+
+  # all other routes will be load our React application
+  # this route definition matches:
+  # - *path: all paths not matched by one of the routes defined above
+  # - constraints:
+  #   - !req.xhr?: it's not a XHR (fetch) request
+  #   - req.format.html?: it's a request for a HTML document
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
